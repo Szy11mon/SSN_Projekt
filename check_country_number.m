f3b906dfb9f5e20756650eeeb2fe5bf8c524a5ab
PawelKepka <40424123+PawelKepka@users.noreply.github.com>
2020-06-17 01:02:59 +0200

    Update check_country_number.m

--- check_country_number.m
+++ check_country_number.m
@@ -2,7 +2,7 @@ previous_country_number = 1;
 for i=1:size(Daily_Stats,1)
     country_name = char(Countries(previous_country_number));
     current_country_number = Daily_Stats(i,1);
-    if strcmp (country_name,'Italy') 
+    if strcmp (country_name,'Poland') 
         italy_number = Daily_Stats(i,1);
     end
     previous_country_number = current_country_number;
